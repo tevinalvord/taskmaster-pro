a744cfba1c54dcd6b42afa5e9b2c96a023e30d6d
tevinalvord <tevin.alvord@gmail.com>
2022-04-12 23:59:24 -0600

    add abiliy to select dates from calendar picker and near/overdue tasks are color coded

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,22 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+
+
+  var taskP = $("<p>").addClass("m-1").text(taskText);
+
 
   // append span and p element to parent li
+
   taskLi.append(taskSpan, taskP);
+
+  
+  // check due date
+
+  auditTask(taskLi);
 
 
   // append to ul list on the page
@@ -117,27 +124,32 @@ $(".list-group").on("click", "span", function() {
 
     // get current text
     
-    var date = $(this)
-
-        .text()
-
-        .trim();
+    var date = $(this).text().trim();
 
 
     // create new input element
 
-    var dateInput = $("<input>")
+    var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-    .attr("type", "text")
-
-    .addClass("form-control")
-
-    .val(date);
-
-
-    // swap out elements
 
     $(this).replaceWith(dateInput);
+
+
+    // enable jQuery ui datepicker
+
+    dateInput.datepicker({
+
+        minDate: 1,
+
+        onClose: function() {
+
+            // when calendar is closed, force a "change" event on the `dateInput`
+
+            $(this).trigger("change");
+
+        }
+
+    });
 
 
     // automatically focus on new element
@@ -148,36 +160,20 @@ $(".list-group").on("click", "span", function() {
 
 // value of due date was changed
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
 
     // get current text
 
-    var date = $(this)
-
-        .val()
-
-        .trim();
-
+    var date = $(this).val().trim();
 
     // get the parent ul's id attribute
 
-    var status = $(this)
-
-        .closest(".list-group")
-
-        .attr("id")
-
-        .replace("list-", "");
+    var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
 
     // get the task's position in the list of other li elements
 
-    var index = $(this)
-
-        .closest(".list-group-item")
-
-        .index();
-
+    var index = $(this).closest(".list-group-item").index();
 
     // update task in array and re-save to localstorage
 
@@ -185,19 +181,18 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
     saveTasks();
 
-
     // recreate span element with bootstrap classes
 
-    var taskSpan = $("<span>")
-
-        .addClass("badge badge-primary badge-pill")
-
-        .text(date);
-
+    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
 
     // replace input with span element
 
     $(this).replaceWith(taskSpan);
+
+
+    // Pass task's <li> element into auditTask() to check new due date
+
+    auditTask($(taskSpan).closest(".list-group-item"));
 
 });
 
@@ -320,6 +315,45 @@ $("#trash").droppable({
 
 });
 
+$("#modalDueDate").datepicker({
+
+    minDate: 1
+
+});
+
+var auditTask = function(taskEl) {
+
+    // get date from task element
+
+    var date = $(taskEl).find("span").text().trim();
+
+
+    // convert to moment object at 5:00pm
+
+    var time = moment(date, "L").set("hour", 17);
+
+
+    // remove any old classes from element
+
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+
+    // apply new class if task is near/over due date
+
+    if (moment().isAfter(time)) {
+
+        $(taskEl).addClass("list-group-item-danger");
+
+    }
+
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+
+        $(taskEl).addClass("list-group-item-warning");
+
+    }
+
+
+};
 
 
 // modal was triggered
